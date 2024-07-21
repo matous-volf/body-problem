a3c77eefba104247741bdfc42901eaf3001669e3
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-07-21 09:49:14 +0200

    feat: create the components for vizualization

--- body_problem_web_app/src/main.rs
+++ body_problem_web_app/src/main.rs
@@ -1,23 +1,6 @@
-use yew::prelude::*;
+mod components;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
-    }
-}
+use components::app::{App};
 
 fn main() {
     yew::Renderer::<App>::new().render();
