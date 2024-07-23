e780f845c155d15fa2f84d7be0e30dfd77ea98f9
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-07-23 20:24:00 +0200

    feat: make the canvas stretch to full width

--- body_problem_web_app/src/components/app.rs
+++ body_problem_web_app/src/components/app.rs
@@ -1,9 +1,10 @@
 use std::f64::consts::PI;
 use std::rc::Rc;
 
+use gloo_events::EventListener;
 use gloo_utils::format::JsValueSerdeExt;
 use nalgebra::Vector2;
-use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, window};
 use web_sys::wasm_bindgen::{JsCast, JsValue};
 use yew::prelude::*;
 use yew_agent::prelude::{use_reactor_subscription, UseReactorSubscriptionHandle};
@@ -65,52 +66,67 @@ pub struct BodyCanvasProps {
 #[function_component(BodyCanvas)]
 pub fn body_canvas(props: &BodyCanvasProps) -> Html {
     let canvas_ref = use_node_ref();
+    let canvas = canvas_ref.cast::<HtmlCanvasElement>();
+    let context = use_state(|| None);
 
-    use_effect_with(
-        canvas_ref.clone(),
-        move |canvas_ref| {
-            if let Some(canvas) = canvas_ref.cast::<HtmlCanvasElement>() {
-                let canvas: HtmlCanvasElement = canvas;
-                let context: CanvasRenderingContext2d = canvas
-                    .get_context_with_context_options("2d", &JsValue::from_serde(&serde_json::json!({
+    {
+        let context = context.clone();
+        // on each change of the canvas (e.g. window resize)
+        use_effect_with(
+            canvas.clone(),
+            move |canvas| {
+                let mut resize_listener = None;
+                if let Some(canvas) = canvas {
+                    let canvas: HtmlCanvasElement = canvas.clone();
+
+                    let initialize_context = Callback::from(move |_: Event| {
+                        canvas.set_width(window().unwrap().inner_width().unwrap().as_f64().unwrap() as u32);
+
+                        let context_new: CanvasRenderingContext2d = canvas
+                            .get_context_with_context_options("2d", &JsValue::from_serde(&serde_json::json!({
                         "alpha": false,
                         "depth": false,
                         "stencil": false,
                     })).unwrap())
-                    .unwrap()
-                    .unwrap()
-                    .dyn_into::<CanvasRenderingContext2d>()
-                    .unwrap();
+                            .unwrap()
+                            .unwrap()
+                            .dyn_into::<CanvasRenderingContext2d>()
+                            .unwrap();
 
-                context.translate((canvas.width() / 2) as f64, (canvas.height() / 2) as f64).unwrap();
+                        context_new.translate((canvas.width() / 2) as f64, (canvas.height() / 2) as f64).unwrap();
+                        context_new.set_fill_style(&"#ffffff".into());
 
-                // context.set_fill_style(&"#000000".into());
-                // context.fill_rect(-((canvas.width() / 2) as f64), -((canvas.height() / 2) as f64), canvas.width() as f64, canvas.height() as f64);
+                        context.set(Some(context_new));
+                    });
 
-                context.set_fill_style(&"#ffffff".into());
-            }
-        },
-    );
+                    initialize_context.emit(Event::new("").unwrap());
 
+                    resize_listener = Some(EventListener::new(
+                        &window().unwrap(),
+                        "resize",
+                        move |e| initialize_context.emit(e.clone()),
+                    ));
+                }
+
+                move || drop(resize_listener)
+            },
+        );
+    }
+
+    // on each change of the bodies
     use_effect_with(
-        (canvas_ref.clone(), props.bodies.clone()),
-        move |(canvas_ref, bodies)| {
-            if let Some(canvas) = canvas_ref.cast::<HtmlCanvasElement>() {
-                let canvas: HtmlCanvasElement = canvas;
-                let context: CanvasRenderingContext2d = canvas
-                    .get_context("2d")
-                    .unwrap()
-                    .unwrap()
-                    .dyn_into::<CanvasRenderingContext2d>()
-                    .unwrap();
+        ((*context).clone(), props.bodies.clone()),
+        move |(context, bodies)| {
+            if let Some(context) = context {
+                if let Some(canvas) = canvas {
+                    context.clear_rect(-((canvas.width() / 2) as f64), -((canvas.height() / 2) as f64), canvas.width() as f64, canvas.height() as f64);
 
-                context.clear_rect(-((canvas.width() / 2) as f64), -((canvas.height() / 2) as f64), canvas.width() as f64, canvas.height() as f64);
-
-                for body in bodies {
-                    context.begin_path();
-                    context.arc(body.position.x, body.position.y, 10f64, 0f64, 2f64 * PI).unwrap();
-                    context.fill();
-                    context.close_path();
+                    for body in bodies {
+                        context.begin_path();
+                        context.arc(body.position.x, body.position.y, 10f64, 0f64, 2f64 * PI).unwrap();
+                        context.fill();
+                        context.close_path();
+                    }
                 }
             }
             || ()
@@ -118,8 +134,8 @@ pub fn body_canvas(props: &BodyCanvasProps) -> Html {
     );
 
     html! {
-        <div>
-            <canvas ref={canvas_ref} width={1000} height={500}/>
+        <div class="w-full">
+            <canvas ref={canvas_ref} height="500"/>
         </div>
     }
 }
