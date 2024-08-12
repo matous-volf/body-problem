f920a21ee0f8870f5465923374647a554eb5ad0d
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-08-12 21:55:37 +0200

    feat: add a link to Wikipedia to the footer

--- body_problem_web_app/src/components/footer.rs
+++ body_problem_web_app/src/components/footer.rs
@@ -4,8 +4,18 @@ use yew::{function_component, html, Html};
 pub fn footer() -> Html {
     html! {
         <footer class="py-4 flex flex-col gap-1 sm:flex-row sm:gap-0 justify-center items-center text-neutral-500 sm:divide-x divide-neutral-500">
-            <span class="sm:pr-5">{"a simulation of the n-body problem"}</span>
-            <span class="sm:pl-5"><a href="https://github.com/matous-volf/body-problem" target="_blank"><i class="fa-brands fa-github mr-2"></i><span class="underline">{"source code"}</span></a></span>
+            <span class="sm:pr-5">
+                {"a simulation of the "}
+                <a class="underline" href="https://en.wikipedia.org/wiki/N-body_problem">
+                    {"n-body problem"}
+                </a>
+            </span>
+            <span class="sm:pl-5">
+                <a href="https://github.com/matous-volf/body-problem" target="_blank">
+                    <i class="fa-brands fa-github mr-2"></i>
+                    <span class="underline">{"source code"}</span>
+                </a>
+            </span>
         </footer>
     }
 }
